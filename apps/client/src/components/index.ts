7f821725294a0e61721b460a1ee2dc4003fdfd6b
gogogadgetscott <gogogadgetscott@gmail.com>
2025-12-21 14:30:39 -0500

    chore: drop legacy component exports, keep React only

--- apps/client/src/components/index.ts
+++ apps/client/src/components/index.ts
@@ -1,23 +1,9 @@
-// Legacy exports (old .ts files)
-export * from "./BookmarkCard.ts";
-export * from "./SkeletonCard.ts";
-export * from "./RichBookmarkCard.ts";
-export * from "./Icon.ts";
-export * from "./Badge.ts";
-export * from "./Button.ts";
-export * from "./Tag.ts";
-export * from "./ViewToggle.ts";
-export * from "./UserProfile.ts";
-export * from "./SelectionUI.ts";
-export * from "./Omnibar.ts";
-export * from "./Header.ts";
-
-// New React components
-export { Button as ReactButton } from './Button.tsx';
-export { Icon as ReactIcon } from './Icon.tsx';
-export { Badge as ReactBadge } from './Badge.tsx';
-export { BookmarkCard as ReactBookmarkCard } from './BookmarkCard.tsx';
-export { Header as ReactHeader } from './Header.tsx';
+// React components
+export { Button } from './Button.tsx';
+export { Icon } from './Icon.tsx';
+export { Badge } from './Badge.tsx';
+export { BookmarkCard } from './BookmarkCard.tsx';
+export { Header } from './Header.tsx';
 export { EmptyState } from './EmptyState.tsx';
 export { Loading, LoadingSpinner, LoadingState } from './Loading.tsx';
-export { Omnibar as ReactOmnibar } from './Omnibar.tsx';
+export { Omnibar } from './Omnibar.tsx';
